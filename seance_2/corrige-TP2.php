6aa7bb8ec21e521fb3c3845f4f520ede4c5444b3
Iren <I23.Stepanyan.Iren@etud.ufar.am>
2025-10-14 20:35:52 +0400

    adding the users in materials, S3 (folder with txt files) and ex15

--- seance_2/corrige-TP2.php
+++ seance_2/corrige-TP2.php
@@ -25,7 +25,7 @@ Exercice 9
 #! /bin/sh
 echo "Il y a `ls -al | grep -c '^-.*\.txt$'` fichier(s) dont le nom se termine par .txt"
 echo "Il y a `ls -al | grep -c '^-.*\.sh$'` fichier(s) dont le nom se termine par .sh"
-echo "Il y a `ls -al | grep -c '^-.* \.'` fichier(s) caché(s)."
+echo "Il y a `ls -al | grep -c '^-.* \.' ` fichier(s) caché(s)."
 echo "Il y a `ls -al | grep -v '\.txt$' | grep -v '\.sh$' | grep -v ' \.' | grep -c '^-'` autre(s) fichier(s)."
 
 Exercice 10
