cf6e008ffbb161a39b0fe2c8625ac29821c8299e
Iren <I23.Stepanyan.Iren@etud.ufar.am>
2025-10-14 19:27:24 +0400

    Adding ex12

---
+++ seance_3/ex12/ex12.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+> f_francais.txt 
+#cree ou vider le fichier de sortie
+
+while read ligne
+do
+	traduit=""
+	sep=""
+	for mot in $ligne
+	do
+		francais=`grep "^$mot " dico.txt | cut -d ' ' -f 2`
+		traduit="${traduit}${sep}${francais}"
+		sep=" "
+	done
+	echo "$traduit" >>f_francais.txt
+done < f_anglais.txt
