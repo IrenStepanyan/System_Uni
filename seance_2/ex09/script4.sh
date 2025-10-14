e31a10dabd12d0590295920447f6c2caaf529922
Iren <I23.Stepanyan.Iren@etud.ufar.am>
2025-10-14 18:07:26 +0400

    Updating ex9

--- seance_2/ex09/script4.sh
+++ seance_2/ex09/script4.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 
-# grep -v smth -> lines that doesn't correspond to the smth
-# ici j'utilise grep -v '^\.' '\.txt$' pour eviter inclusion the file qui se termine par .txt ou qui commence avec point (fichieres caches) 
 
-txt_count=$(ls -al 2>/dev/null | grep '^-' | grep '\.txt$' | wc -l)
-echo "Il y a $txt_count fichiers dont le nom se termine par .txt"
-
-sh_count=$(ls -al 2>/dev/null | grep '^-' | grep '\.sh$' | wc -l)
-echo "Il y a $sh_count fichiers dont le nom se termine par .sh"
-
-caches_count=$(ls -al 2>/dev/null | grep '^-' | grep '^\.[^.]' | wc -l)
-echo "Il y a $caches_count fichiers caches"
-# [^ smth] caractere autre qu'un smth
-# grep '^\.[^.]' -> les lignes qui commerencent par un point et suivi d'un cararactere autre qu'un point (.) 
-
-autre_count=$(ls -al 2>/dev/null | grep '^-' | grep -v '\.txt$' | grep -v '\.sh$' | grep -v '^\.' | wc -l)
-echo "Il y a $autre_count autres fichiers"
+echo "Il y a `ls -al | grep -c '^-.*\.txt$' ` fichier(s) dont le nom se termine par .txt"
+echo "Il y a  `ls -al | grep -c '^-.*\.sh$' ` fichier(s) dont le nom se termine par .sh"
+echo "Il y a `ls -al | grep -c '^-.*\.' ` fichier(s) cache(s)"
+echo "Il y a `ls -al | grep -v '\.txt$' | grep -v '\.sh$' | grep -v '\.' | grep -c '^-' ` autre(s) fichier(s)." 
 
